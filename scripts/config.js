8a5ebc64966d92a78f032f0c38dc3b8b6e3cc4ee
Rosmadec <102622069+Rosmadec@users.noreply.github.com>
2024-12-15 15:30:37 +0100

    First commit

---
+++ scripts/config.js
@@ -0,0 +1,16 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ * 
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+
+const listeMots = ["a", "a", "a"]
+const listePhrases = ["b", "b", "b","b"]
+
+/* 
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+const listePhrases = ["Pas de panique !", "La vie, l'univers et le reste", "Merci pour le poisson"]
+*/
---
+++ scripts/popup_toto.js
@@ -0,0 +1,47 @@
+/*********************************************************************************
+ *
+ * Ce fichier contient toutes les fonctions nécessaires à l'affichage et à la
+ * fermeture de la popup de partage.
+ *
+ *********************************************************************************/
+
+/**
+ * Cette fonction affiche la popup pour partager son score.
+ */
+function afficherPopup() {
+  let popupBackground = document.querySelector(".popupBackground")
+  // La popup est masquée par défaut (display:none), ajouter la classe "active"
+  // va changer son display et la rendre visible.
+  popupBackground.classList.add("active");
+}
+
+/**
+ * Cette fonction cache la popup pour partager son score.
+ */
+function cacherPopup() {
+  let popupBackground = document.querySelector(".popupBackground")
+  // La popup est masquée par défaut (display:none), supprimer la classe "active"
+  // va rétablir cet affichage par défaut.
+  popupBackground.classList.remove("active");
+}
+
+/**
+ * Cette fonction initialise les écouteurs d'événements qui concernent
+ * l'affichage de la popup.
+ */
+function initAddEventListenerPopup() {
+  // On écoute le click sur le bouton "partager"
+  let btnZonePartage = document.querySelector(".zonePartage button");
+  // Quand on a cliqué sur le bouton partagé, on affiche la popup
+  btnZonePartage.addEventListener("click", () => {
+    afficherPopup();
+  });
+
+  // On écoute le click sur la div "popupBackground"
+  let popupBackground = document.querySelector(".popupBackground")
+  // Si on a cliqué précisément sur la popupBackground
+  // (et pas un autre élément qui se trouve dedant)
+
+  // Alors on cache la popup
+  cacherPopup()
+}
